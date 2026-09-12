e68cc444d08037400ac91308477d7e7adbca01e9
Shivaprabhu <40115160+shivaprabhu@users.noreply.github.com>
2026-09-12 17:04:09 +0530

    + typography

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <section id="center">
         <div className="hero">
           <h1 style={{ fontSize: "2.5rem", marginBottom: "15px" }}>biomarkHQ</h1>
-          <p style={{ fontSize: "0.9rem" }}>Personalised longitudinal biomarker tracking and evidence-backed guidance</p>
+          <p style={{ fontSize: "0.95rem" }}>Personalised longitudinal biomarker tracking and evidence-backed guidance</p>
         </div>
       </section>
     </>
